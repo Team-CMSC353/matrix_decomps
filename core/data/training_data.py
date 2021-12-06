67386b431bd646a8e87a9ec99fb45467c4ca20a2
Christine Ibaraki <christine.ibaraki@gmail.com>
2021-12-06 11:17:20 -0600

    udpate docstrings for data wrapper

--- core/data/training_data.py
+++ core/data/training_data.py
@@ -1,4 +1,6 @@
-import os
+"""
+Wrapper for sampling data, computing idf, storing related matrices
+"""
 import gensim.corpora as corpora
 from core.data.arxiv_data_io import *
 from core.data.text.tf_idf_helpers import *
@@ -7,19 +9,19 @@ from gensim.models.coherencemodel import CoherenceModel
 
 class TrainingData:
     """
-    Wrapper for training data and related matrices
+    Training data and related matrices
     """
 
     def __init__(self, path_to_pkl_data):
         self.data_df = pd.read_pickle(path_to_pkl_data)
-
         self.train_df, _ = sample_arxiv_data_by_category(self.data_df)
 
+        # compute tf-idf matrix on train tokens
         self.tokens = self.train_df['tokens']
         tfidf_obj, self.index_to_word = fit_tfidf(self.train_df['tokens'])
         self.tfidf_train_matrix, self.index_to_doc = transform_tfidf(self.train_df, tfidf_obj=tfidf_obj)
 
-        # for coherence
+        # data for computing coherence on topics model
         self.input_data = self.train_df['tokens'].tolist()
         self.id2word = corpora.Dictionary(self.input_data)
         self.corpus = [self.id2word.doc2bow(text) for text in self.input_data]
@@ -27,6 +29,10 @@ class TrainingData:
     def compute_coherence(self, topic_list):
         """
         Return coherence for topic model trained on self.train_df
+
+        ONLY FOR TOPIC MODELING OF THIS DATA SET
+        THIS COMPUTATION CAN BE EXTREMELY SLOW
+
         :param topic_list: list of list of terms
         :return: (float) coherence
         """
